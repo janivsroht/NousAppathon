6b7beb39685241d0da9c54ffe06e7b9d09844abc
janivsroht <janivvin@gmail.com>
2025-11-17 20:17:49 +0530

    Changed to my name

--- app/src/main/java/com/example/nousappathon/MainActivity.kt
+++ app/src/main/java/com/example/nousappathon/MainActivity.kt
@@ -4,18 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.nousappathon.ui.theme.NousAppathonTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
+
+
+
+
+
+
         enableEdgeToEdge()
         setContent {
             NousAppathonTheme {
@@ -24,6 +42,7 @@ class MainActivity : ComponentActivity() {
                         name = "Janiv",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    ColorWheelSplash()
                 }
             }
         }
@@ -38,10 +57,39 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun ColorWheelSplash() {
+
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.orange)
+    )
+
+    // Animate indefinitely
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LottieAnimation(
+            composition = composition,
+            progress = progress,
+            modifier = Modifier.size(245.dp) // change size as needed
+        )
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NousAppathonTheme {
         Greeting("Android")
+        ColorWheelSplash()
     }
 }
--- app/src/main/java/com/example/nousappathon/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nousappathon/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -51,7 +52,9 @@ fun NousAppathonTheme(
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = lightColorScheme(
+            background = Color(0xFFF9F6EE) // set your background color here
+        ),
         typography = Typography,
         content = content
     )
